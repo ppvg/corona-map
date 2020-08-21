183ed2d9e7cfb85d4e511864dfa2e06de7ab486f
Jeroen Breen <>
2020-08-21 10:43:07 +0200

    add sewage plants

---
+++ src/components/sewage-treatment-plants/rna-levels-graph.vue
@@ -0,0 +1,25 @@
+<script>
+    export default {
+        name: 'rna-levels-graph',
+        components: {},
+        props: {},
+        computed: {},
+        methods: {}
+    }
+</script>
+
+
+<template>
+    <div class="rna-levels-graph">
+        rna-levels-graph
+    </div>
+</template>
+
+
+<style lang="scss">
+    @import '@/styles/variables.scss';
+
+    .rna-levels-graph {
+
+    }
+</style>
