9c4808f1fbc3c9e87adbf08e623fe03cc1beb7ee
Jeroen Breen <>
2020-08-26 00:55:07 +0200

    add indentation

--- src/data/ggds.js
+++ src/data/ggds.js
@@ -1,3 +1,79 @@
-const ggds = [{"ggd_code":"GG0706","title":"GGD Drenthe"},{"ggd_code":"GG3406","title":"GGD Amsterdam"},{"ggd_code":"GG1413","title":"GGD Noord- en Oost-Gelderland"},{"ggd_code":"GG7206","title":"GGD Fryslân"},{"ggd_code":"GG4816","title":"Dienst Gezondheid & Jeugd ZHZ"},{"ggd_code":"GG4607","title":"GGD Rotterdam-Rijnmond"},{"ggd_code":"GG2707","title":"GGD Hollands-Noorden"},{"ggd_code":"GG1106","title":"GGD Regio Twente"},{"ggd_code":"GG2209","title":"GGD Flevoland"},{"ggd_code":"GG4506","title":"GGD Hollands-Midden"},{"ggd_code":"GG5206","title":"GGD West-Brabant"},{"ggd_code":"GG2514","title":"GGD Regio Utrecht"},{"ggd_code":"GG0111","title":"GGD Groningen"},{"ggd_code":"GG1911","title":"Veiligheids- en Gezondheidsregio Gelderland-Midden"},{"ggd_code":"GG5608","title":"GGD Brabant-Zuidoost"},{"ggd_code":"GG6106","title":"GGD Zuid-Limburg"},{"ggd_code":"GG7306","title":"GGD Zaanstreek/Waterland"},{"ggd_code":"GG6011","title":"GGD Limburg-Noord"},{"ggd_code":"GG2014","title":"GGD Gelderland-Zuid"},{"ggd_code":"GG5406","title":"GGD Hart voor Brabant"},{"ggd_code":"GG3109","title":"GGD Kennemerland"},{"ggd_code":"GG3606","title":"GGD Gooi en Vechtstreek"},{"ggd_code":"GG5006","title":"GGD Zeeland"},{"ggd_code":"GG1009","title":"GGD IJsselland"},{"ggd_code":"GG7014","title":"GGD Haaglanden"}];
+const ggds = [{
+    "ggd_code": "GG0706",
+    "title": "GGD Drenthe"
+}, {
+    "ggd_code": "GG3406",
+    "title": "GGD Amsterdam"
+}, {
+    "ggd_code": "GG1413",
+    "title": "GGD Noord- en Oost-Gelderland"
+}, {
+    "ggd_code": "GG7206",
+    "title": "GGD Fryslân"
+}, {
+    "ggd_code": "GG4816",
+    "title": "Dienst Gezondheid & Jeugd ZHZ"
+}, {
+    "ggd_code": "GG4607",
+    "title": "GGD Rotterdam-Rijnmond"
+}, {
+    "ggd_code": "GG2707",
+    "title": "GGD Hollands-Noorden"
+}, {
+    "ggd_code": "GG1106",
+    "title": "GGD Regio Twente"
+}, {
+    "ggd_code": "GG2209",
+    "title": "GGD Flevoland"
+}, {
+    "ggd_code": "GG4506",
+    "title": "GGD Hollands-Midden"
+}, {
+    "ggd_code": "GG5206",
+    "title": "GGD West-Brabant"
+}, {
+    "ggd_code": "GG2514",
+    "title": "GGD Regio Utrecht"
+}, {
+    "ggd_code": "GG0111",
+    "title": "GGD Groningen"
+}, {
+    "ggd_code": "GG1911",
+    "title": "Veiligheids- en Gezondheidsregio Gelderland-Midden"
+}, {
+    "ggd_code": "GG5608",
+    "title": "GGD Brabant-Zuidoost"
+}, {
+    "ggd_code": "GG6106",
+    "title": "GGD Zuid-Limburg"
+}, {
+    "ggd_code": "GG7306",
+    "title": "GGD Zaanstreek/Waterland"
+}, {
+    "ggd_code": "GG6011",
+    "title": "GGD Limburg-Noord"
+}, {
+    "ggd_code": "GG2014",
+    "title": "GGD Gelderland-Zuid"
+}, {
+    "ggd_code": "GG5406",
+    "title": "GGD Hart voor Brabant"
+}, {
+    "ggd_code": "GG3109",
+    "title": "GGD Kennemerland"
+}, {
+    "ggd_code": "GG3606",
+    "title": "GGD Gooi en Vechtstreek"
+}, {
+    "ggd_code": "GG5006",
+    "title": "GGD Zeeland"
+}, {
+    "ggd_code": "GG1009",
+    "title": "GGD IJsselland"
+}, {
+    "ggd_code": "GG7014",
+    "title": "GGD Haaglanden"
+}
+];
 
 export default ggds;
