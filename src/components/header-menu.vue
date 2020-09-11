c724a2d0074c779393194731eb00949c1a8bbb43
Jeroen Breen <>
2020-09-11 22:21:06 +0200

    design

--- src/components/header-menu.vue
+++ src/components/header-menu.vue
@@ -56,6 +56,8 @@
         align-items: center;
         background: #fff;
         box-shadow: 0 2px 8px rgba(0,0,0,0.2);
+        position: relative;
+        z-index: 1;
 
         .title {
             height: 100%;
