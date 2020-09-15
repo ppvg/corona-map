3ba170552c60a484aac09b99aa6d333df530cd65
Jeroen Breen <>
2020-09-15 14:39:35 +0200

    Update main.js

--- data-conversion-stuff/threshold-report/main.js
+++ data-conversion-stuff/threshold-report/main.js
@@ -2,7 +2,7 @@ let url, mainThreshold, reportThresholds;
 
 url = 'https://raw.githubusercontent.com/mzelst/covid-19/master/data/municipality-totals.csv';
 mainThreshold = 50;
-reportThresholds = [5,10,15,20,25,30,35,40,45];
+reportThresholds = [0, 5,10,15,20,25,30,35,40,45];
 
 
 d3.csv(url)
@@ -14,10 +14,13 @@ d3.csv(url)
         reports += '\n';
 
         for (let item of data) {
-            let report = analyseCity(item);
-            if (report) {
-                reports += report;
+            if (item.Municipality_name === 'Delfzijl') {
+                let report = analyseCity(item);
+                if (report) {
+                    reports += report;
+                }
             }
+
         }
         console.log(reports);
     })
@@ -55,11 +58,12 @@ function analyseCity(item) {
     }
     firstMainThresholdIndex = findFirstThreshold(dates, population, mainThreshold);
     if (firstMainThresholdIndex) {
+        let datesBeforeThreshold = dates.slice(0, (firstMainThresholdIndex + 1));
         report += dates[firstMainThresholdIndex].date + ',';
         report.mainThreshold = dates[firstMainThresholdIndex].date;
         report.history = [];
         for (let threshold of reportThresholds) {
-            let index = findFirstThreshold(dates, population, threshold);
+            let index = findLastThreshold(datesBeforeThreshold, population, threshold);
             report += (firstMainThresholdIndex - index) + ','
         }
         report += '\n';
@@ -85,6 +89,32 @@ function findFirstThreshold(dates, population, threshold) {
         }
     }
     return null;
+}
+
+function findLastThreshold(dates, population, threshold) {
+    let correctedThreshold, nextThreshold, correctedNextThreshold, set, lastIndex, thresholdIndex;
+    lastIndex = null;
+    thresholdIndex = reportThresholds.indexOf(threshold);
+    if (thresholdIndex < reportThresholds.length) {
+        nextThreshold =  reportThresholds[thresholdIndex + 1];
+        correctedNextThreshold = nextThreshold * population / 100000;
+    }
+
+    correctedThreshold = threshold * population / 100000;
+
+    set = [];
+    for (let date of dates) {
+        set.push(date.newCases);
+        if (set.length > 7) {
+            set.shift();
+        }
+        let total = count(set);
+        if (total > correctedThreshold && (total < correctedNextThreshold || !nextThreshold)) {
+            lastIndex = dates.indexOf(date);
+        }
+    }
+
+    return lastIndex;
 }
 
 function count(set) {
